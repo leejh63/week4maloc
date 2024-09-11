3f24240d681e945412f3c46b1fa6463e181a1a3b
leejh63 <leejh63@gmail.com>
2024-09-11 16:51:41 +0000

    modified:   mm.c

--- mm.c
+++ mm.c
@@ -1,14 +1,56 @@
 /*
- * mm-naive.c - The fastest, least memory-efficient malloc package.
- * 
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
- *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
- */
+/// 버디 시스템 로직-- 수정중 --////
+
+- 블럭 사이즈 
+사이즈의 경우 주소값을 통해서 구할 수 있다. 이외에도 다른 방법 존재할 것 같다.
+
+- 연결 함수
+기존 가용리스트 처음위치에 연결하는 함수와 큰 차이 없음
+
+- 갱신 함수
+기존 갱신방법과 큰 차이 없음
+
+- 버디 주소 찾기 함수(블럭의 주소, 블럭의 사이즈)
+만약 사이즈*2 % 주소 == 0 이면 
+    (주소 + 사이즈)
+아니면
+    (주소 - 사이즈)
+찾은 버디의 주소를 반환해준다.
+
+1. 초기
+현재 max 20MB 이므로 
+max_heap_size = (1 << 23) + wsize*(1 + 1 + 24 + 1 + 1 \)
+- 프롤로그헤더, 풋터, 힙 시작, 끝, 사이즈 클래스 총 24개 (2^0 ~ 2^23)
+확장 후 23클래스에 가용 블럭과 next 포인터를 통해 연결  
+가용블럭 헤더생성 후 헤더에 사이즈 및 할당여부 갱신(갱신은 함수로)
+
+2. 할당 진행
+특정 사이즈 n을 받았다고 가정한다면, (n의 최대치는 이미 정해져있음)
+
+사이즈 n+1을 해준 뒤 진행
+
+일단 사이즈 클래스 탐색 진행
+만약 적절한 사이즈 클래스에 가용블럭 발견시 할당진행
+
+없다면 반복 진행, 
+초기 "n의 크기와 가용 블럭 사이즈 비교 > n이 작다면 블럭을 1/2로 분할"
+분할시 오른쪽 가용블럭의 헤더의 포인터를 통해 사이즈에 맞는 클래스에 연결(연결은 함수로)
+"왼쪽 가용블럭과 n의 크기 비교 > n이 작다면 블럭을 1/2로 크기 분할"
+.... 계속 반복진행
+진행 중 n이 2^k보다 커진다면 2^(k+1) 블럭할당진행
+// 최소사이즈 도달시 그냥 할당?
+할당 진행시 할당블럭 헤더에 사이즈 및 할당 여부 갱신 진행(갱신은 함수로)
+
+3. 할당 해제
+특정 블럭을 할당 해제 할 경우 병합(병합은 함수로)진행 후 헤더정보 수정 후
+맞는 사이즈 클래스와 연결
+
+4. 병합
+버디 찾은후(함수로) 찾은 버디의 주소값을 통해 사이즈 확인 현 블럭의 사이즈와 같다면 
+병합진행
+이후 병합된 블럭의 버디를 찾은 후 병합 여부 확인 > 무한 반복
+반복 진행 중 병합이 불가능 하다면 반복을 멈춘다.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
