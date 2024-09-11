936bd99225292a03e6de9dea2cec3090a778f2ce
leejh63 <leejh63@gmail.com>
2024-09-11 03:51:59 +0000

    modified:   mm.c

--- mm.c
+++ mm.c
@@ -39,26 +39,31 @@ team_t team = {
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp)-DSIZE)))
 static char *heap_listp;
-static char *free_listp;
-void remove_in_freelist(void *bp);
+void removefreelist(void *bp);
 static void *extend_heap(size_t words);
 void *mm_malloc(size_t size);
 static void place(void *bp, size_t asize);
 static void *find_fit(size_t asize);
 static int free_pos(size_t asize);
-/* for explicit*/
-// 이중 포인터 미사용
+
 #define ADBL(bp) (*(void**)(bp))
-// 이중 포인터 사용
-// #define PREV_FREEP(bp) (*(void **)(bp))
-// #define NEXT_FREEP(bp) (*(void **)(bp + WSIZE))
+
+/*///////////////////////////////////////////////////////////////////
+안쓰는 상수 및 매크로 정리안함
+현재 free 함수 미구현, 추가적인 함수 구현 후 및 에러 확인 필요
+일부분 통과 확인
+*////////////////////////////////////////////////////////////////////
+
 /*  mm_init - initialize the malloc package  */
 int mm_init(void)
 { /* Create the initial empty heap */
+
     if ((heap_listp = mem_sbrk(16 * WSIZE)) == (void *)-1)
         return -1;
     PUT(heap_listp, 0);
     PUT(heap_listp + (1 * WSIZE), PACK(DSIZE * 2, 1));
+
+    // 범위는 16~32, 32~64, 64~128... 이런식으로 지정
     for (int i = 2; i <= 12; i++)
     {
         PUT(heap_listp + (i * WSIZE), NULL);
@@ -69,39 +74,38 @@ int mm_init(void)
     heap_listp += (2 * WSIZE);
     return 0;
 }
-/*
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
- */
+
 void *mm_malloc(size_t size)
 {
     size_t asize;
     size_t extendsize;
     char *bp;
     void *bpp;
-    if (size == 0)
+
+    if (size == 0){
         return NULL;
-    if (size <= DSIZE)
-        asize = 2 * DSIZE;
-    else
-        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
-    if ((bpp = find_fit(asize)) != NULL)
-    {
-        bp = ADBL(bpp);
-        // if (bp != NULL){
-        //     remove_in_freelist(bpp);
-        //     return bp;
-        // }
     }
-    // extendsize = MAX(1 << free_pos(asize), CHUNKSIZE);
+
+    if (size <= DSIZE){
+        asize = 2 * DSIZE;
+    } 
+    else{
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE);
+    }
+
+    if ((bpp = find_fit(asize)) != NULL){
+        bp = ADBL(bpp);
+    }
+
+    // 범위가 2^n 단위 따라서 비트 연산가능
     extendsize = 1 << (free_pos(asize));
     if ((bp = extend_heap(extendsize / WSIZE)) == NULL) {
         return NULL;
     }
     return bp;
 }
-static void* extend_heap(size_t words)
-{
+
+static void* extend_heap(size_t words){
     char* bp;
     size_t size;
     size = (words % 2) ? (words+1)*WSIZE : words*WSIZE;
@@ -109,54 +113,61 @@ static void* extend_heap(size_t words)
         return NULL;
     return bp;
 }
-void mm_free(void *ptr)
-{
+
+void mm_free(void *ptr){ 
+    // free를 진행하면서 받은 가용사이즈에 맞게 분할하는 로직을 생각함.
+    // 구현은 패스
     size_t size = GET_SIZE(HDRP(ptr));
-    // PUT(HDRP(ptr), PACK(size, 0));
-    // PUT(FTRP(ptr), PACK(size, 0));
 }
-//------------
+
 static int free_pos(size_t asize){
     int n = 0;
     unsigned int power = 1;
+
     while (power < asize) {
         power <<= 1;
         n += 1;
-        if (n >= 12)
-            break;
+        // free 함수 구현 후 아래 조건 적용
+        // if (n >= 12)
+        //     break;
     }
     return n;
 }
-/////////////////////////////////////
-static void *find_fit(size_t asize) //
+
+static void *find_fit(size_t asize)
 {
     void* block_pointer;
     int n = free_pos(asize);
     block_pointer = heap_listp + ((n) * WSIZE);
-    // 위치제대로 되는지 다시한번 검증
-    if (block_pointer == NULL){// 역참조?
+
+    if (block_pointer == NULL){
         return NULL;
     }
     return block_pointer;
 }
-/////////////////////////
-void remove_in_freelist(void *bp)
-{
+
+void removefreelist(void *bp){
     ADBL(bp) = ADBL(ADBL(bp));
 }
-//-------------
+
 void *mm_realloc(void *ptr, size_t size)
 {
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
+
     newptr = mm_malloc(size);
+
     if (newptr == NULL)
       return NULL;
+
     copySize = *(size_t *)((char *)oldptr - WSIZE);
     if (size < copySize)
       copySize = size;
+
     memcpy(newptr, oldptr, copySize);
+
     mm_free(oldptr);
+
     return newptr;
 }
